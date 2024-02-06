fffa4158697aeef1e6adf7237221f21286532f0b
Bronislav <shevchenko-bg@yandex.ru>
2024-02-06 20:26:02 +0300

    added class laptop and toString method

--- Laptop.java
+++ Laptop.java
@@ -1,4 +1,13 @@
 public class Laptop {
-    
+    String colour;
+    String brand;
+    Integer CPUtype;
+    Integer amountOfRAM;
+    Integer displaySize;
+
+    @Override
+    public String toString() {
+        return "Brand: " + brand + ", Colour: " + colour + ", Display, \": " + displaySize + ", CPU type: " + CPUtype + ", Amount of RAM: " + amountOfRAM;
+    }
 }
 
