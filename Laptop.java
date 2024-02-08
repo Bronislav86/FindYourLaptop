3014c0ef41743d32419b311e9eea53d057145a4e
Bronislav <shevchenko-bg@yandex.ru>
2024-02-08 22:50:25 +0300

    added cases 1 and 2

--- Laptop.java
+++ Laptop.java
@@ -15,5 +15,53 @@ public class Laptop {
     public int hashCode() {
         return brand.hashCode() + 5*colour.hashCode() + displaySize*11 + CPUtype.hashCode()*18 + amountOfHDD*19 + amountOfRAM*9;
     }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Integer getAmountOfHDD() {
+        return amountOfHDD;
+    }
+
+    public void setAmountOfHDD(Integer amountOfHDD) {
+        this.amountOfHDD = amountOfHDD;
+    }
+
+    public Integer getAmountOfRAM() {
+        return amountOfRAM;
+    }
+
+    public void setAmountOfRAM(Integer amountOfRAM) {
+        this.amountOfRAM = amountOfRAM;
+    }
+
+    public String getCPUtype() {
+        return CPUtype;
+    }
+
+    public void setCPUtype(String cPUtype) {
+        CPUtype = cPUtype;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+    
+    public Integer getDisplaySize() {
+        return displaySize;
+    }
+
+    public void setDisplaySize(Integer displaySize) {
+        this.displaySize = displaySize;
+    }
 }
 
--- Main.java
+++ Main.java
@@ -20,15 +20,22 @@
 4. Работа выполнена только для каких то конкретных ноутбуков, и если поменять характеристики ноутбуков или добавить еще ноутбук, то программа начинает работать некорректно
  */
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Main {
+    //private static Laptop findParameters = new Laptop("null", "null", 0, "null", 0, 0);
 
     public static void main(String[] args) {
+
+        //Laptop laptop6 = new Laptop("Mac", "Grey", 16, "Intel", 16, 1000);
+
         Laptop laptop1 = new Laptop();
         laptop1.brand = "Huawei";
         laptop1.colour = "Grey";
@@ -59,7 +66,7 @@ public class Main {
         laptop4.displaySize = 17;
         laptop4.CPUtype = "AMD";
         laptop4.amountOfRAM = 16;
-        laptop4.amountOfHDD = 1000;
+        laptop4.amountOfHDD = 128;
 
         Laptop laptop5 = new Laptop();
         laptop5.brand = "VAIO";
@@ -76,27 +83,146 @@ public class Main {
         laptops.add(laptop4);
         laptops.add(laptop5);
 
+        Map<Integer, String> mapBrands = new HashMap<>();
+        mapBrands.put(1, laptop1.getBrand());
+        mapBrands.put(2, laptop2.getBrand());
+        mapBrands.put(3, laptop3.getBrand());
+        mapBrands.put(4, laptop4.getBrand());
+        mapBrands.put(6, laptop5.getBrand());
+
+        Map<Integer, String> mapColours = new HashMap<>();
+        mapColours.put(1, "Grey");
+        mapColours.put(2, "Black");
+        mapColours.put(3, "Red");
+        mapColours.put(4, "White");
+
+        // List<Object> CPUtype = new LinkedList<>();
+        // CPUtype.add(laptop1.getCPUtype());
+        // CPUtype.add(laptop2.getCPUtype());
+        // CPUtype.add(laptop3.getCPUtype());
+        // CPUtype.add(laptop4.getCPUtype());
+        // CPUtype.add(laptop5.getCPUtype());
+
+        // List<Object> displays = new LinkedList<>();
+        // displays.add(14);
+        // displays.add(15);
+        // displays.add(16);
+        // displays.add(17);
+
+        // List<Object> ram = new LinkedList<>();
+        // ram.add(128);
+        // ram.add(256);
+        // ram.add(512);
+        // ram.add(1000);
+
+        // List<Object> hdd = new LinkedList<>();
+        // hdd.add(128);
+        // hdd.add(256);
+        // hdd.add(512);
+        // hdd.add(1000);
+
+        // Map<String, LinkedList<Object>> parameters = new HashMap<>();
+        // parameters.putIfAbsent("Производитель", (LinkedList<Object>) brands);
+        // parameters.putIfAbsent("Цвет", (LinkedList<Object>) colours);
+        // parameters.putIfAbsent("Процессор", (LinkedList<Object>) CPUtype);
+        // parameters.putIfAbsent("Экран", (LinkedList<Object>) displays);
+        // parameters.putIfAbsent("ОЗУ", (LinkedList<Object>) ram);
+        // parameters.putIfAbsent("Память", (LinkedList<Object>) hdd);
+
+        // parameters.get("Производитель");
+
+
+        //System.out.println(parameters);
+        findBy(laptops, mapBrands, mapColours);
     }
 
-    static HashMap<Object, Object> findBy(){
-        HashMap<Object, Object> resLaptops = new HashMap<>();
+    static void findBy(Set<Laptop> laptops, Map<Integer, String> mapBrands, Map<Integer, String> mapColours){
+        System.out.println("\nПредставляем вашему вниманию имеющийся ассортимент ноутбуков: \n");
+        printSet(laptops);
+        Map<String, Object> findMap = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите пункт меню: " +
-                            "1. Производитель" +
-                            "2. Цвет" + 
-                            "3. Диагональ экрана" + 
-                            "4. Тип процессора" + 
-                            "5. Объем оперативной памяти" + 
-                            "6. Объем жесткого диска");
-        int menuNum = scanner.nextLine();
-        
-        return resLaptops;
+        System.out.printf("Чтобы подобрать устройство для себя введите цифру, соответствующую критерию поиска: \n");
+        menu();
+        boolean work = true;
+        while (work) {
+            String menuNo  = scanner.nextLine();
+                       
+            switch (menuNo) {
+                case "1":
+                    System.out.println("Выберите производителя из списка: ");
+                    for (var item : mapBrands.entrySet()) {
+                        System.out.printf("%d. %s\n", item.getKey(), item.getValue());
+                    }
+                    int numOfBrend = Integer.parseInt(scanner.nextLine());                    
+                    findMap.put("1", mapBrands.get(numOfBrend));
+                    for (var findItem : findMap.entrySet()) {
+                        System.out.printf("%s ", findItem.getValue());
+                    }
+                    System.out.println("\n\nВведите следующую команду: ");
+                    break;
+                case "2":
+                    System.out.println("Выберите цвет из списка: \n");
+                    for (var item : mapColours.entrySet()) {
+                        System.out.printf("%s. %s\n", item.getKey(), item.getValue());
+                    }
+                    int numOfColour = Integer.parseInt(scanner.nextLine());
+                    findMap.put("2", mapColours.get(numOfColour));
+                    for (var findItem : findMap.entrySet()) {
+                        System.out.printf("%s ", findItem.getValue());
+                    }
+                    System.out.println("\n\nВведите следующую команду:");
+                    menu();
+                    break;
+                case "3":
+                    
+                    break;
+                case "4":
+                    
+                    break;
+                case "5":
+                    
+                    break;
+                case "6":
+                    
+                    break;
+                case "7":
+                    work = false;
+                    break;
+            
+                default :
+                    System.out.println("Команда не распознана. " + 
+                                        "Выберите один из пунктов меню:\n");
+                    menu();
+                    menuNo = scanner.nextLine();
+                    break;
+            }        
+        }
+        scanner.close();
+        filter();
     } 
+
+    private static void menu() {
+        System.out.println("\n1. Производитель\n" +
+                            "2. Цвет\n" + 
+                            "3. Диагональ экрана\n" + 
+                            "4. Тип процессора\n" + 
+                            "5. Объем оперативной памяти\n" + 
+                            "6. Объем жесткого диска\n" + 
+                            "7. Выполнить Поиск");
+    }
+
+    private static void filter() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'filter'");
+    }
 
     private static void printSet(Set<Laptop> set) {
         for (Laptop laptop : set) {
-            System.out.println(laptop);
+            System.out.printf("%s \n", laptop);
         }
+        System.out.println();
     }
+
+    
 
 }
